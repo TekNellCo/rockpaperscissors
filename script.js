ca72d0095afed48bcf1009597109b6f3ec73c237
NellyNell <129366078+TekNellCo@users.noreply.github.com>
2023-07-02 21:15:05 -0400

    add highligh to img

--- script.js
+++ script.js
@@ -1,6 +1,73 @@
-const r = "Rock";
-const p = "Paper";
-const s = "Scissors";
+
+const  paperBtn = document.getElementById('paper');
+const rockBtn = document.getElementById('rock');
+const scissorsBtn = document.getElementById('scissors')
+
+const computerChoice = document.querySelector('.computerChoice');
+const playerChoice = document.querySelector('.playerChoice');
+const result = document.querySelector('.result');
+
+let p = 'Paper';
+let s = 'Scissors' ;
+let r = 'Rock';
+
+///scissorsBtn.addEventListener('click',scissor);
+//rockBtn.addEventListener('click',rock);
+///paperBtn.addEventListener('click',paper);
+
+
+
+
+function scissors(){ 
+    playerChoice.textContent = 'Player choice : Scissors';
+    if (choice() == r){
+        computerChoice.textContent = 'Computer choice : Rock';
+        result.textContent = "Result : Computer wins!";
+
+    }else if( choice()== p){
+        computerChoice.textContent = 'Computer choice : Paper';
+        result.textContent = "Result : You win!";
+
+    } else {
+        computerChoice.textContent = 'Computer choice : Scissors';
+        result.textContent = "Result : Tie!";
+    }
+}
+
+function rock(){
+    playerChoice.textContent = 'Player choice : Rock';
+    if (choice()== s){
+        computerChoice.textContent = 'Computer choice : Scissors';
+        result.textContent = "Result : You win!";
+
+    }else if( choice()== p){
+        computerChoice.textContent = 'Computer choice : Paper';
+        result.textContent = "Result : Computer wins!";
+
+    } else {
+        computerChoice.textContent = 'Computer choice : Rock';
+        result.textContent = "Result : Tie!";
+    }
+}
+
+function paper(){
+    playerChoice.textContent = 'Player choice : Paper';
+
+    if (choice()== r){
+        computerChoice.textContent = 'Computer choice : Rock';
+        result.textContent = "Result : You win!";
+
+    }else if( choice()== s){
+        computerChoice.textContent = 'Computer choice : Scissors';
+        result.textContent = "Result : Computer wins!";
+
+    } else {
+        computerChoice.textContent = 'Computer choice : Paper';
+        result.textContent = "Result : Tie!";
+    }
+}
+
+
 
 
 
@@ -18,43 +85,24 @@ function choice(){
       return s}
 }
 
-function player(){
-    let answer = prompt('rock, paper, or scissors?');
-    if (answer === 'rock'){
-        return r
-    } else if (answer === 'paper'){
-        return p
-    } else if (answer= 'scissors'){
-        return s
-    }    
-}
-
-function game(){
-    playerSelection = player();
-    computerSelection = choice();
-    if (playerSelection === computerSelection){
-        return "Oh wow, we tied"
-    } else if (computerSelection === r && playerSelection === p){
-        return "Dang beat me with paper!"
-    } else if (computerSelection === p && playerSelection === s){
-        return "Dang you got me with scissors!"
-    } else if (computerSelection === s && playerSelection === r){
-        return "Rock beats scissors, you win."
-    } 
-    else if (computerSelection === r && playerSelection === s){
-        return "Rock beats scissor :D!"
-    } else if (computerSelection === p && playerSelection === r){
-        return "I win, paper beats rock"
-    } else if (computerSelection === s && playerSelection === r){
-        return "Sorry scissors trumps paper"
-    }
-
-    
-}
 
 
 
-console.log (game())
+
+////function player(){
+ //   let answer = prompt('rock, paper, or scissors?');
+  //  if (answer === 'rock'){
+ //       return r
+ //   } else if (answer === 'paper'){
+ //       return p
+//    } else if (answer= 'scissors'){
+ //       return s
+//    }    
+///}
+
+
+
+/////
 
 
 
